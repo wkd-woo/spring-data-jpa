94d599a87c35a18bb6e9722da36b7885d71f7a6a
wkd-woo <wkdwoos@gmail.com>
2022-02-23 16:25:46 +0900

    반환 타입 => Collection?

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -8,6 +8,7 @@ import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.entity.Team;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -139,6 +140,17 @@ class MemberRepositoryTest {
         for (MemberDto dto : memberDto) {
             System.out.println("dto = " + dto);
         }
-
     }
+
+    @Test
+    void findByNames(){
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        memberRepository.findByNames(Arrays.asList("AAA", "BBB"));
+    }
+
 }
