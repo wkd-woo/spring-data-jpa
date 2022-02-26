0cfb876b74cee092718b977f54fbfe75f344b9a8
wkd-woo <wkdwoos@gmail.com>
2022-02-26 19:21:28 +0900

    JPA Hint and Lock

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -264,4 +264,33 @@ class MemberRepositoryTest {
         }
     }
 
+    @Test
+    void queryHint() {
+        //given
+        Member member1 = new Member("member1", 10);
+
+        memberRepository.save(member1);
+        em.flush();
+        em.clear();
+
+        //when
+        Member findMember = memberRepository.findReadOnlyByUsername("member1");
+        findMember.setUsername("member2");
+
+        em.flush();
+    }
+
+    @Test
+    void lock() {
+        //given
+        Member member1 = new Member("member1", 10);
+        memberRepository.save(member1);
+        em.flush();
+        em.clear();
+
+        //when
+        List<Member> result = memberRepository.findLockByUsername("member1");
+    }
+
+
 }
