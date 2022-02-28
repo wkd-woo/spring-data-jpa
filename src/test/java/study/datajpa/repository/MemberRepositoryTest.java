70db846948e0d0229572efdf92064203913b7f8c
wkd-woo <wkdwoos@gmail.com>
2022-02-28 22:50:21 +0900

    사용자 정의 리포지토리 구현

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -292,5 +292,10 @@ class MemberRepositoryTest {
         List<Member> result = memberRepository.findLockByUsername("member1");
     }
 
+    @Test
+    void callCustom(){
+        List<Member> result = memberRepository.findMemberCustom();
+
+    }
 
 }
