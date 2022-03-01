f76e506f3232015e77c91b6d862b8a161fa9b844
wkd-woo <wkdwoos@gmail.com>
2022-03-01 16:02:03 +0900

    도메인 클래스 컨버터 + paging & sort

--- src/main/java/study/datajpa/dto/MemberDto.java
+++ src/main/java/study/datajpa/dto/MemberDto.java
@@ -2,6 +2,7 @@ package study.datajpa.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import study.datajpa.entity.Member;
 
 
 @Data
@@ -11,4 +12,9 @@ public class MemberDto {
     private Long id;
     private String username;
     private String teamName;
+
+    public MemberDto(Member member) {
+        this.id = member.getId();
+        this.username = member.getUsername();
+    }
 }
